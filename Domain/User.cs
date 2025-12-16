0e4be8fad4728aae8edf970a54b02f06a3119052
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-16 14:28:04 -0700

    Change UserID property to ID with int type

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,5 +1,5 @@
 public class User {
-  public string UserID { get; set; }
+  public int ID { get; set; }
   public string Username { get; set; }
   public string Password { get; set; }
   public string TimeZone { get; set; }
