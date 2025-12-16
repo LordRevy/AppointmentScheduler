f16aa4cda863a9825396be9f91f4e7be1661e9e3
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-16 07:02:47 -0700

    Add AppointmentID and UserID properties to Appointment

--- Domain/Appointment.cs
+++ Domain/Appointment.cs
@@ -1,4 +1,6 @@
 public class Appointment {
+  public int AppointmentID { get; set; }
+  public int UserID { get; set; }
   public int CustomerID { get; set; }
   public string AppointmentType { get; set; }
   public DateTime StartTime { get; set; }
