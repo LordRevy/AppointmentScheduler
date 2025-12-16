b2dbfe3095f327030c7230e93122aed456502a83
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-16 06:50:53 -0700

    Create Appointment class with properties

---
+++ Domain/Appointment.cs
@@ -0,0 +1,6 @@
+public class Appointment {
+  public int CustomerID { get; set; }
+  public string AppointmentType { get; set; }
+  public DateTime StartTime { get; set; }
+  Public DateTime EndTime { get; set; }
+}
