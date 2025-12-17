5badf5099c2360299abfec5ab1b483d8f19b8fdc
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-17 07:14:30 -0700

    Fix casing of 'Public' to 'public'

--- Domain/Appointment.cs
+++ Domain/Appointment.cs
@@ -4,5 +4,5 @@ public class Appointment {
   public int CustomerID { get; set; }
   public string AppointmentType { get; set; }
   public DateTime StartTime { get; set; }
-  Public DateTime EndTime { get; set; }
+  public DateTime EndTime { get; set; }
 }
