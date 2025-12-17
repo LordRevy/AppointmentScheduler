0a6199e2e1f10ada1a43600a0f1431dcce5a9200
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-17 07:15:33 -0700

    Rename CustomerID property to ID

--- Domain/Customer.cs
+++ Domain/Customer.cs
@@ -1,5 +1,5 @@
 public class Customer {
-  public int CustomerID { get; set; }
+  public int ID { get; set; }
   public string Name { get; set; }
   public string Address { get; set; }
   public string Phone { get; set: }
