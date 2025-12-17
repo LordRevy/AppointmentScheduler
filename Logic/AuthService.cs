cab430f6b7f864bc612e92bf2effed4c6394e457
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-17 07:32:29 -0700

    Adding namespace to AuthService

--- Logic/AuthService.cs
+++ Logic/AuthService.cs
@@ -1,27 +1,30 @@
 using AppointmentScheduler.Domain;
 using System.Collections.Generic;
 
-public class AuthService
+namespace AppointmentScheduler.Logic
 {
-  private readonly List<User> userList = new List<User>                     // TODO: Replace with a JSON-based User store.
-  {                                                                         // Passwords should not be stored in plain english for real world applications. Use Hashing + Salting.
-    new User
-    {
-      ID = 12345,
-      Username = "test",
-      Password = "test"
-    }
-  };
-  
-  public bool ValidateCredentials(string username, string password)
+  public class AuthService
   {
-    foreach(var user in userList)
-    {
-      if(user.Username == username && user.Password == password)
+    private readonly List<User> userList = new List<User>                     // TODO: Replace with a JSON-based User store.
+    {                                                                         // Passwords should not be stored in plain english for real world applications. Use Hashing + Salting.
+      new User
       {
-        return true;
+        ID = 12345,
+        Username = "test",
+        Password = "test"
       }
+    };
+    
+    public bool ValidateCredentials(string username, string password)
+    {
+      foreach(var user in userList)
+      {
+        if(user.Username == username && user.Password == password)
+        {
+          return true;
+        }
+      }
+      return false;
     }
-    return false;
   }
 }
