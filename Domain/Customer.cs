72154861bb4bb8cd2f7e14b7946604826c57d890
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-17 07:24:04 -0700

    Refactor Customer class into AppointmentScheduler namespace

--- Domain/Customer.cs
+++ Domain/Customer.cs
@@ -1,6 +1,10 @@
-public class Customer {
-  public int ID { get; set; }
-  public string Name { get; set; }
-  public string Address { get; set; }
-  public string Phone { get; set: }
+namespace AppointmentScheduler.Domain
+{
+  public class Customer
+  {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Phone { get; set: }
+  }
 }
