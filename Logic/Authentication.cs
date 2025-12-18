00c567df64f5c346643f5120b8769de2fe2ecc1f
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-18 14:18:24 -0700

    Restarting the project with better file structure
    I got confused with each class responsibilities, so im going to detail that out and start again

---
+++ Logic/Authentication.cs
@@ -0,0 +1,12 @@
+using AppointmentScheduler.Domain;
+using System;
+using System.Data.Odbc;
+
+namespace AppointmentScheduler.Logic
+{
+    public class Authentication
+    {
+        // Call UserRepository to validate login
+        // Return User if successful
+    }
+}
