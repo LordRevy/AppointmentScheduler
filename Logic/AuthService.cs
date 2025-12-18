03b9154efa384611fece7f310fcdff1b8dc66cd7
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-18 07:13:32 -0700

    Refactor AuthService to use database.

--- Logic/AuthService.cs
+++ Logic/AuthService.cs
@@ -1,30 +1,47 @@
 using AppointmentScheduler.Domain;
-using System.Collections.Generic;
+using System;
+using System.Data.Odbc;
 
 namespace AppointmentScheduler.Logic
 {
   public class AuthService
   {
-    private readonly List<User> userList = new List<User>                     // TODO: Replace with a JSON-based User store.
-    {                                                                         // Passwords should not be stored in plain english for real world applications. Use Hashing + Salting.
-      new User
-      {
-        ID = 12345,
-        Username = "test",
-        Password = "test"
-      }
-    };
-    
-    public bool ValidateCredentials(string username, string password)
+    private readonly string connectionString =
+        "Driver={MySQL ODBC 8.0 Driver};Server=127.0.0.1;Port=3306;Database=client_schedule;User=sqlUser;Password=Passw0rd!;";
+
+    public User? ValidateCredentials(string username, string password)
     {
-      foreach(var user in userList)
+      using (var conn = new OdbcConnection(connectionString))
       {
-        if(user.Username == username && user.Password == password)
+        conn.Open();
+        string sql = "SELECT userId, userName, password, active, createDate, createdBy, lastUpdate, lastUpdateBy " +
+                     "FROM user WHERE userName = ? AND password = ? AND active = 1";
+
+        using (var cmd = new OdbcCommand(sql, conn))
         {
-          return true;
+          cmd.Parameters.AddWithValue("@userName", username); // Replaces the ? placeholders above.
+          cmd.Parameters.AddWithValue("@password", password); // Prevents SQL injection.
+
+          using (var reader = cmd.ExecuteReader())
+          {
+            if (reader.Read())
+            {
+              return new User
+              {
+                UserId = reader.GetInt32(0),
+                UserName = reader.GetString(1),
+                Password = reader.GetString(2),
+                Active = reader.GetBoolean(3),
+                CreateDate = reader.GetDateTime(4),
+                CreatedBy = reader.GetString(5),
+                LastUpdate = reader.GetDateTime(6),
+                LastUpdateBy = reader.GetString(7)
+              };
+            }
+          }
         }
       }
-      return false;
+      return null; // login failed.
     }
   }
 }
