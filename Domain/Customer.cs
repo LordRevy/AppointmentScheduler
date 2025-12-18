0b668f1523d2ebba3f6901aa77f228df057e5b62
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-18 09:34:48 -0700

    Format Customer class for consistency

--- Domain/Customer.cs
+++ Domain/Customer.cs
@@ -1,14 +1,14 @@
 namespace AppointmentScheduler.Domain
 {
-  public class Customer
-  {
-    public int CustomerId { get; set; }
-    public string CustomerName { get; set; }
-    public int AddressId { get; set; }
-    public bool Active { get; set; }
-    public DateTime CreateDate { get; set; }
-    public string CreatedBy { get; set; }
-    public DateTime LastUpdate { get; set; }
-    public string LastUpdateBy { get; set; }
-  }
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public int AddressId { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string LastUpdateBy { get; set; }
+    }
 }
