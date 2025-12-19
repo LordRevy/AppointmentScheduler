c02b8462d1d41421f6c1d23b8b1a4e1919c023de
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-19 06:15:05 -0700

    Changed placeholder to "?" for better understanding

--- Data/UserRepository.cs
+++ Data/UserRepository.cs
@@ -19,7 +19,7 @@ namespace AppointmentScheduler.Data
                     
                     using (var cmd = new OdbcCommand(sql, conn))
                     {
-                        cmd.Parameters.AddWithValue("@userName", username); // Replaces the ? placeholders above. Prevents SQL injection.
+                        cmd.Parameters.AddWithValue("?", username); // Replaces the ? placeholders above. Prevents SQL injection.
                         
                         using (var reader = cmd.ExecuteReader())
                         {
@@ -30,7 +30,7 @@ namespace AppointmentScheduler.Data
                                     UserId = reader.GetInt32(0),
                                     UserName = reader.GetString(1),
                                     Password = reader.GetString(2),
-                                    Active = reader.GetBoolean(3),
+                                    Active = reader.GetByte(3) == 1, // Gets the 0 or 1 and compares it to 1. If true it evaluates to the boolean True, if not, it becomes False.
                                     CreateDate = reader.GetDateTime(4),
                                     CreatedBy = reader.GetString(5),
                                     LastUpdate = reader.GetDateTime(6),
