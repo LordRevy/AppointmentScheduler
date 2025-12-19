2297bb0dcce4744bccf33bd295291d5bef28009e
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-19 13:25:57 -0700

    Update LoginRecord.cs

--- Logic/LoginRecord.cs
+++ Logic/LoginRecord.cs
@@ -1,19 +1,18 @@
 using System;
 using System.IO;
 
-namespace AppointmentScheduler.Logic
+namespace AppointmentScheduler.Logic;
+
+public class LoginRecord
 {
-    public class LoginRecord
+    private const string FileName = "Records/LoginHistory.txt";
+    private readonly string message;
+
+    public void Log(string username, bool result)
     {
-        private const string FileName = "Records/LoginHistory.txt";
-        private readonly string message;
-    
-        public void Log(string username, bool result)
-        {
-            DateTime loginTime = DateTime.Now;
-            message = $"Login Attempt | Username: {username} | Time: {loginTime:u} | Success? {result}";
-          
-            File.AppendAllText(FileName, message + Environment.NewLine);
-        }
+        DateTime loginTime = DateTime.Now;
+        message = $"Login Attempt | Username: {username} | Time: {loginTime:u} | Success? {result}";
+      
+        File.AppendAllText(FileName, message + Environment.NewLine);
     }
 }
