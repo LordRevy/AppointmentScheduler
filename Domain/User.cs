c77f4935c2505d6c053a8b999aab69e3c2cf148d
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-19 13:25:01 -0700

    Update User.cs

--- Domain/User.cs
+++ Domain/User.cs
@@ -1,14 +1,13 @@
-namespace AppointmentScheduler.Domain
+namespace AppointmentScheduler.Domain;
+
+public class User
 {
-    public class User
-    {
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public bool Active { get; set; }
-        public DateTime CreateDate { get; set; }
-        public string CreatedBy { get; set; }
-        public DateTime LastUpdate { get; set; }
-        public string LastUpdateBy { get; set; }
-    }
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool Active { get; set; }
+    public DateTime CreateDate { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public string LastUpdateBy { get; set; }
 }
