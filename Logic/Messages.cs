b5ee3131f97008c6d49e0b28f2ddd88800d2e861
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-19 13:26:18 -0700

    Update Messages.cs

--- Logic/Messages.cs
+++ Logic/Messages.cs
@@ -1,32 +1,31 @@
 using AppointmentScheduler.Domain;
 using System.Collections.Generic;
 
-namespace AppointmentScheduler.Logic
+namespace AppointmentScheduler.Logic;
+
+public class Messages
 {
-    public class Messages
+    private readonly Dictionary<string, Dictionary<string, string>> messages =
+        new Dictionary<string, Dictionary<string, string>>
     {
-        private readonly Dictionary<string, Dictionary<string, string>> messages =
-            new Dictionary<string, Dictionary<string, string>>
+        ["en"] = new Dictionary<string, string>
         {
-            ["en"] = new Dictionary<string, string>
-            {
-                ["LoginSuccess"] = "Login successful!",
-                ["InvalidCredentials"] = "Invalid username or password."
-            },
-            ["ru"] = new Dictionary<string, string>
-            {
-                ["LoginSuccess"] = "вход выполнен успешно!",
-                ["InvalidCredentials"] = "Неверное имя пользователя или пароль."
-            }
-        };
-        
-        public string LoginMessage(User user, bool result)
+            ["LoginSuccess"] = "Login successful!",
+            ["InvalidCredentials"] = "Invalid username or password."
+        },
+        ["ru"] = new Dictionary<string, string>
         {
-            if (result)
-            {
-                return messages[user.Language]["LoginSuccess"];
-            }
-            return messages[user.Language]["InvalidCredentials"];
+            ["LoginSuccess"] = "вход выполнен успешно!",
+            ["InvalidCredentials"] = "Неверное имя пользователя или пароль."
         }
+    };
+    
+    public string LoginMessage(User user, bool result)
+    {
+        if (result)
+        {
+            return messages[user.Language]["LoginSuccess"];
+        }
+        return messages[user.Language]["InvalidCredentials"];
     }
 }
