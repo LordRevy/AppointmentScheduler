28fdce04500fa756d7c5c6acb5161475a2491cda
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-19 13:12:21 -0700

    Refactor Repository class and add GetConnection method
    Refactor GetRow method to remove unused code and add GetConnection method.

--- Data/Repository.cs
+++ Data/Repository.cs
@@ -3,17 +3,8 @@ public abstract class Repository
     protected readonly string connectionString =
         "Driver={MySQL ODBC 8.0 Driver};Server=127.0.0.1;Port=3306;Database=client_schedule;User=sqlUser;Password=Passw0rd!;";
 
-    protected T? GetRow<T>(string sql, Func<OdbcDataReader, T> map, params object[] parameters)
+    protected OdbcConnection GetConnection()
     {
-        using var conn = new OdbcConnection(connectionString);
-        conn.Open();
-
-        using var cmd = new OdbcCommand(sql, conn);
-
-        foreach (var p in parameters)
-            cmd.Parameters.AddWithValue("?", p);
-
-        using var reader = cmd.ExecuteReader();
-        return reader.Read() ? map(reader) : default;
+        return new OdbcConnection(connectionString);
     }
 }
