71d892844b72533a5c8b5d8facc6ad7090152d08
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-29 12:57:59 -0700

    Create Address.cs

---
+++ Domain/Address.cs
@@ -0,0 +1,8 @@
+namespace AppointmentScheduler.Domain;
+
+public class Address
+{
+  public int AddressId { get; set; }
+  public string Address { get; set; }
+  public string Phone { get; set; }
+}
