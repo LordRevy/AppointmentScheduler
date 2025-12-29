4f7e3851d10464fd88dbd69e93a68d10de8535d3
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-29 14:45:12 -0700

    Update AppointmentRepository.cs
    still need to change CRUD parameters to appointment layout instead of customer

--- Data/AppointmentRepository.cs
+++ Data/AppointmentRepository.cs
@@ -6,14 +6,26 @@ namespace AppointmentScheduler.Data;
 
 public class AppointmentRepository : Repository
 {
+    private const string GetSql = "SELECT * FROM appointment WHERE appointmentId = ?";
+    private const string InsertSql = @"INSERT INTO appointment (
+        appointmentId, customerId, userId, title, description, location,
+        contact, type, url, start, end, createDate, createdBy, lastUpdate, lastUpdateBy
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+    private const string UpdateSql = @"UPDATE appointment SET 
+        appointmentId = ?, customerId = ?, userId = ?, title = ?, description = ?, location = ?,
+        contact = ?, type = ?, url = ?, start = ?, end = ?, lastUpdate = ?, lastUpdateBy = ?
+        WHERE appointmentId = ?";
+        
+    private const string DeleteSql = "DELETE FROM appointment WHERE appointmentId = ?";
+
     public Appointment? GetAppointment(int ID)
     {
-        const string sql = "SELECT * FROM customer WHERE appointmentId = ?";
-
         using var conn = GetConnection();
         conn.Open();
 
-        using var cmd = new OdbcCommand(sql, conn);
+        using var cmd = new OdbcCommand(GetSql, conn);
         cmd.Parameters.AddWithValue("?", ID);
 
         using var reader = cmd.ExecuteReader();
@@ -40,4 +52,49 @@ public class AppointmentRepository : Repository
             LastUpdateBy = reader.GetString(14)
         };
     }
+
+        public void AddAppointment(Appointment appointment)
+    {
+        using var conn = GetConnection();
+        conn.Open();
+
+        using var cmd = new OdbcCommand(InsertSql, conn);
+        cmd.Parameters.AddWithValue("?", customer.CustomerName);
+        cmd.Parameters.AddWithValue("?", customer.AddressId);
+        cmd.Parameters.AddWithValue("?", customer.Active ? 1 : 0);
+        cmd.Parameters.AddWithValue("?", customer.CreateDate);
+        cmd.Parameters.AddWithValue("?", customer.CreatedBy);
+        cmd.Parameters.AddWithValue("?", customer.LastUpdate);
+        cmd.Parameters.AddWithValue("?", customer.LastUpdateBy);
+
+        cmd.ExecuteNonQuery();
+    }
+
+    public void UpdateAppointment(Appointment appointment)
+    {
+        using var conn = GetConnection();
+        conn.Open();
+
+        using var cmd = new OdbcCommand(UpdateSql, conn);
+        cmd.Parameters.AddWithValue("?", customer.CustomerName);
+        cmd.Parameters.AddWithValue("?", customer.AddressId);
+        cmd.Parameters.AddWithValue("?", customer.Active ? 1 : 0);
+        cmd.Parameters.AddWithValue("?", customer.LastUpdate);
+        cmd.Parameters.AddWithValue("?", customer.LastUpdateBy);
+        cmd.Parameters.AddWithValue("?", customer.CustomerId);
+
+        cmd.ExecuteNonQuery();
+    }
+
+    public void DeleteAppointment(Appointment appointment)
+    {
+        using var conn = GetConnection();
+        conn.Open();
+
+        using var cmd = new OdbcCommand(DeleteSql, conn);
+        cmd.Parameters.AddWithValue("?", customer.CustomerId);
+
+        cmd.ExecuteNonQuery();
+    }
+}
 }
