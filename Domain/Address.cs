74ab55c16f0074c500a4d1687b07820f71b7e48f
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-30 07:10:00 -0700

    Update Address.cs

--- Domain/Address.cs
+++ Domain/Address.cs
@@ -2,7 +2,14 @@ namespace AppointmentScheduler.Domain;
 
 public class Address
 {
-  public int AddressId { get; set; }
-  public string Address { get; set; }
-  public string Phone { get; set; }
+    public int AddressId { get; set; }
+    public string Address { get; set; }
+    public string Address2 { get; set; } // Null if there is no second aaddress
+    public int CityId { get; set; }
+    public string PostalCode { get; set; }
+    public string Phone { get; set; }
+    public DateTime CreateDate { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public string LastUpdateBy { get; set; }
 }
