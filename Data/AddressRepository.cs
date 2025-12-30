9fce6fb525fd5c8195fc018cb6d7fc12d047e6b7
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2025-12-30 06:58:30 -0700

    Create AddressRepository.cs

---
+++ Data/AddressRepository.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Data.Odbc;
+using AppointmentScheduler.Domain;
+
+namespace AppointmentScheduler.Data;
+
+public class AddressRepository : Repository
+{
+    private string GetSql = "SELECT * FROM address WHERE addressId = ?";
+
+    public Address? GetAddress(int addressId)
+    {
+        using var conn = GetConnection();
+        conn.Open();
+
+        using var cmd = new OdbcCommand(GetSql, conn);
+        cmd.Parameters.AddWithValue("?", addressId);
+
+        using var reader = cmd.ExecuteReader();
+
+        if (!reader.Read())
+            return null;
+
+        return new Address
+        {
+            AddressId = reader.GetInt32(0), // Replace indexes with GetOrdinal
+            Address = reader.GetString(1),
+            Address2 = reader.IsDBNull(2) ? null : reader.GetString(2),
+            CityId = reader.GetInt32(3),
+            PostalCode = reader.GetString(4),
+            Phone = reader.GetString(5),
+            CreateDate = reader.GetDateTime(6),
+            CreatedBy = reader.GetString(7),
+            LastUpdate = reader.GetDateTime(8),
+            LastUpdateBy = reader.GetString(9)
+        };
+    }
+}
