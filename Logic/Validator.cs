8b3e6b311cdfce1fdb6bb52a4ef960ec69aced29
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-06 14:47:26 -0700

    Create Validator.cs

---
+++ Logic/Validator.cs
@@ -0,0 +1,17 @@
+namespace AppointmentScheduler.Logic;
+
+public class Validator
+{
+  public bool CheckIfPhone(object phone)
+  {
+    
+  }
+  public bool CheckIfDateTime(object date)
+  {
+    
+  }
+  public bool CheckIfNull(object dataItem)
+  {
+    
+  }
+}
