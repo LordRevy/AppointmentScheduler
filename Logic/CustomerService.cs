cb148b13eb6d79363808806c1aafbc85295c8b11
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-12 08:32:51 -0700

    Update CustomerService.cs

--- Logic/CustomerService.cs
+++ Logic/CustomerService.cs
@@ -5,29 +5,30 @@ namespace AppointmentScheduler.Logic;
 
 public class CustomerService
 {
-    private readonly CustomerRepository _customerRepository = new CustomerRepository();
-    private readonly Validator _validator = new Validator();
+    private readonly CustomerRepository _customerRepository;
+    private readonly AddressService _addressService;
+    private readonly Validator _validator;
+    
+    public CustomerService(CustomerRepository customerRepository, AddressService addressService, Validator validator)
+    {
+        _customerRepository = customerRepository;
+        _addressService = addressService;
+        _validator = validator;
+    }
     
     public bool Create(User user, string customerName, string address, string phone)
     {
-        customerName = customerName?.Trim();
-        address = address?.Trim();
-        phone = phone?.Trim();
     
-        if (customerName == null || address == null || phone == null)
+        if (_validator.CheckIfEmpty(customerName, address, phone))
             throw new ArgumentException("Please do not leave any field empty.");
 
-        addressService.AddAddress(address, phone);
-        addressId = addressService.GetAddressId(address);
+        _addressService.AddAddress(address, phone);
+        var addressId = _addressService.GetAddressId(address);
 
-        if (addressId == null)
-            return false;
-
-        var createDate = DateTime.Now();
+        var createDate = DateTime.Now;
         
         var customer = new Customer
         {
-            CustomerId = ,
             CustomerName = customerName,
             AddressId = addressId,
             Active = true,
@@ -39,6 +40,38 @@ public class CustomerService
     
         _customerRepository.AddCustomer(customer);
     
+        return true;
+    }
+
+    public Customer? GetCustomer(string customerId)
+    {
+        return _customerRepository.GetCustomer(customerId);
+    }
+
+    public bool UpdateCustomer(User user, Customer customer, string customerName, int addressId, bool active)
+    {   
+        customer.CustomerName = customerName;
+        customer.AddressId = addressId;
+        customer.Active = active;
+        customer.LastUpdate = DateTime.Now;
+        customer.LastUpdateBy = user.UserId;
+    
+        _customerRepository.UpdateCustomer(customer);
+    
+        return true;
+    }
+
+    public bool DeleteCustomer(Customer customer)
+    {
+        try
+        {
+            _customerRepository.DeleteCustomer(customer);
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+        
         return true;
     }
 }
