0e81556e8fc52d23ae951c6105278126956c8f90
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-20 09:18:39 -0700

    Remove non-essential properties from User class
    Removed sensitive fields from User class.

--- Domain/User.cs
+++ Domain/User.cs
@@ -4,10 +4,4 @@ public class User
 {
     public int UserId { get; set; }
     public string UserName { get; set; }
-    public string Password { get; set; }
-    public bool Active { get; set; }
-    public DateTime CreateDate { get; set; }
-    public string CreatedBy { get; set; }
-    public DateTime LastUpdate { get; set; }
-    public string LastUpdateBy { get; set; }
 }
