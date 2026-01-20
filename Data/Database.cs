ed0cbff712940301ac6091b4fe12e651ddba45ca
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-20 09:46:41 -0700

    Refactor connection string handling in Database.cs

--- Data/Database.cs
+++ Data/Database.cs
@@ -5,11 +5,11 @@ namespace AppointmentScheduler.Data;
 
 public abstract class Repository
 {
-    protected readonly string connectionString =
-        "Driver={MySQL ODBC 8.0 Driver};Server=127.0.0.1;Port=3306;Database=client_schedule;User=sqlUser;Password=Passw0rd!;";
+    private readonly string _connectionString =
+        ConfigurationManager.ConnectionStrings["ClientScheduleDb"].ConnectionString;
 
     protected OdbcConnection GetConnection()
     {
-        return new OdbcConnection(connectionString);
+        return new OdbcConnection(_connectionString);
     }
 }
