324c50e1eabf017b014dbd4afd792933b53b3e94
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-21 13:57:04 -0700

    Add XML documentation to Database methods
    Added XML documentation for GetConnection and ExecuteNonQuery methods.

--- Data/Database.cs
+++ Data/Database.cs
@@ -8,8 +8,26 @@ public abstract class Repository
     private readonly string _connectionString =
         ConfigurationManager.ConnectionStrings["ClientScheduleDb"].ConnectionString;
 
+/// <summary>
+/// Establishes the connection string to the Database.
+/// </summary>
     protected OdbcConnection GetConnection()
     {
         return new OdbcConnection(_connectionString);
+    }
+
+/// <summary>
+/// Method to replace the ? placeholders with user inputs before executing a non-query.
+/// Takes the SQL string, database connection, and a list of user inputs as parameters.
+/// </summary>
+    protected static void ExecuteNonQuery(string sql, OdbcConnection conn, params object?[] parameters)
+    {
+        using var cmd = new OdbcCommand(sql, conn);
+    
+        foreach (var p in parameters)
+        {
+            cmd.Parameters.AddWithValue("", p);
+        }
+        cmd.ExecuteNonQuery();
     }
 }
