b7a01d984a85d75ded555718b46aee0764b8eb40
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-21 12:22:15 -0700

    Refactor Appointment class properties
    Updated properties in Appointment class to include StartUtc and EndUtc while removing some existing properties.

--- Domain/Appointment.cs
+++ Domain/Appointment.cs
@@ -2,15 +2,11 @@ namespace AppointmentScheduler.Domain;
 
 public class Appointment
 {
-    public int AppointmentId { get; set; }
-    public int CustomerId { get; set; }
-    public int UserId { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Location { get; set; }
-    public string Contact { get; set; }
-    public string Type { get; set; }
-    public string Url { get; set; }
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
+    public int AppointmentId = { get; set; },
+    public int CustomerId = { get; set; },
+    public int UserId = { get; set; },
+    public string Title = { get; set; },
+    public string Type = { get; set; },
+    public DateTime StartUtc = { get; set; },
+    public DateTime EndUtc = { get; set; }
 }
