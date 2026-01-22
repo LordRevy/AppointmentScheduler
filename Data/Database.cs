bd65b601941c8574468db89af1a4a9ab18223225
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-22 06:45:39 -0700

    Fix XML documentation formatting in Database.cs

--- Data/Database.cs
+++ Data/Database.cs
@@ -8,18 +8,18 @@ public abstract class Repository
     private readonly string _connectionString =
         ConfigurationManager.ConnectionStrings["ClientScheduleDb"].ConnectionString;
 
-/// <summary>
-/// Establishes the connection string to the Database.
-/// </summary>
+    /// <summary>
+    /// Establishes the connection string to the Database.
+    /// </summary>
     protected OdbcConnection GetConnection()
     {
         return new OdbcConnection(_connectionString);
     }
 
-/// <summary>
-/// Method to replace the ? placeholders with user inputs before executing a non-query.
-/// Takes the SQL string, database connection, and a list of user inputs as parameters.
-/// </summary>
+    /// <summary>
+    /// Method to replace the ? placeholders with user inputs before executing a non-query.
+    /// Takes the SQL string, database connection, and a list of user inputs as parameters.
+    /// </summary>
     protected static void ExecuteNonQuery(string sql, OdbcConnection conn, params object?[] parameters)
     {
         using var cmd = new OdbcCommand(sql, conn);
