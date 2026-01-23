ce83472b0a419df273cf4a275b0e427ed8e8dc2e
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-01-23 13:25:52 -0700

    Rename Id to UserId and add additional properties

--- Domain/User.cs
+++ Domain/User.cs
@@ -2,6 +2,9 @@ namespace AppointmentScheduler.Domain;
 
 public class User
 {
-    public int Id { get; set; }
+    public int UserId { get; set; }
     public string UserName { get; set; }
+    public string Culture { get; set; }
+    public string Region { get; set; }
+    public string Timezone {get; set; }
 }
