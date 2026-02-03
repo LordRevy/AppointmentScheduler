c0cd1c060732b158d1dc5971dd58633471b71fb6
Trevor Boyce <110129327+LordRevy@users.noreply.github.com>
2026-02-03 07:00:17 -0700

    Update Appointment.cs

--- Domain/Appointment.cs
+++ Domain/Appointment.cs
@@ -2,11 +2,11 @@ namespace AppointmentScheduler.Domain;
 
 public class Appointment
 {
-    public int AppointmentId = { get; set; }
-    public int CustomerId = { get; set; }
-    public int UserId = { get; set; }
-    public string Title = { get; set; }
-    public string Type = { get; set; }
-    public DateTime StartUtc = { get; set; }
-    public DateTime EndUtc = { get; set; }
+    public int AppointmentId { get; set; }
+    public int CustomerId { get; set; }
+    public int UserId { get; set; }
+    public string Title { get; set; }
+    public string Type { get; set; }
+    public DateTime StartUtc { get; set; }
+    public DateTime EndUtc { get; set; }
 }
